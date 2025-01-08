211f6f0a6f9f13fef4a19fff037461b1dc90a90a
MasterIlidan <segamil2001@gmail.com>
2025-01-09 02:51:40 +0500

    Initial commit

---
+++ src/main/java/ru/students/resourseservicediplomproject/ResourseServiceDiplomProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.students.resourseservicediplomproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourseServiceDiplomProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourseServiceDiplomProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/students/resourseservicediplomproject/ResourseServiceDiplomProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.students.resourseservicediplomproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourseServiceDiplomProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
