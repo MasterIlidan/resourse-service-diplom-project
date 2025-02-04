4b4eef2d69e99196864ad235c38a701c45f48d70
MasterIlidan <segamil2001@gmail.com>
2025-02-04 18:17:03 +0500

    Get -> POST. Возврат вместо имен UUID.

--- src/main/java/ru/students/resourseservicediplomproject/controller/ResourceController.java
+++ src/main/java/ru/students/resourseservicediplomproject/controller/ResourceController.java
@@ -4,15 +4,28 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @Controller
 public class ResourceController {
 
-    @GetMapping("/registerNewResource")
-    public ResponseEntity<String> registerNewResource() {
-        log.info("Registered new resource");
-        return new ResponseEntity<>("register new resource", HttpStatus.CREATED);
+    @PostMapping("/registerNewResource")
+    public ResponseEntity<List<String>> registerNewResource(@RequestParam("image") MultipartFile[] images) {
+        List<String> stringList = new ArrayList<>(images.length);
+        for (MultipartFile file:images) {
+            String name = String.valueOf(UUID.randomUUID());
+            stringList.add(name);
+            log.info("Registered new resource {}", name);
+        }
+
+        return new ResponseEntity<>(stringList, HttpStatus.CREATED);
     }
+
 }
