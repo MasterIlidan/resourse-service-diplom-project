d90b6320adf081c9f64bf1653c86a6c60eeec5d6
MasterIlidan <segamil2001@gmail.com>
2025-12-13 16:20:39 +0500

    Тест отправки и получения изображения

--- src/test/java/ru/students/resourseservicediplomproject/controller/ResourceControllerTest.java
+++ src/test/java/ru/students/resourseservicediplomproject/controller/ResourceControllerTest.java
@@ -1,7 +1,10 @@
 package ru.students.resourseservicediplomproject.controller;
 
 import jakarta.transaction.Transactional;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
+import org.postgresql.shaded.com.ongres.scram.common.bouncycastle.base64.Base64;
+import org.postgresql.shaded.com.ongres.scram.common.bouncycastle.base64.Base64Encoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,12 +18,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import ru.students.resourseservicediplomproject.service.ResourceService;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -34,10 +40,7 @@ class ResourceControllerTest {
     ResourceService resourceService;
 
     @Test
-    @Transactional
-    @Rollback
     void registerNewResource_returnWithStatusCreated() throws Exception {
-
         Resource fileResource = new ClassPathResource("./testImage.png");
         MockMultipartFile firstFile = new MockMultipartFile(
                 "image", fileResource.getFilename(),
@@ -64,8 +67,39 @@ class ResourceControllerTest {
     }
 
     @Test
-    void getResource_returnWithStatusOk() {
+    void getResource_returnWithStatusOk() throws Exception {
+        Resource fileResource = new ClassPathResource("./testImage.png");
+        MockMultipartFile firstFile = new MockMultipartFile(
+                "image", fileResource.getFilename(),
+                MediaType.MULTIPART_FORM_DATA_VALUE,
+                fileResource.getInputStream());
+        AtomicReference<String> uuidTestForRemoval = new AtomicReference<>();
+        try {
+            mockMvc.perform(multipart("/resource")
+                            .file(firstFile))
+                    .andExpectAll(
+                            status().isCreated(),
+                            (res) -> {
+                                assertDoesNotThrow(() -> {
+                                    UUID.fromString(res.getResponse().getContentAsString());
+                                });
+                            },
+                            (res) -> {
+                                uuidTestForRemoval.set(res.getResponse().getContentAsString());
+                            });
 
+            mockMvc.perform(
+                    get("/resource" + uuidTestForRemoval.get())
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpectAll(
+                            status().isOk(),
+                            content().contentType(new MediaType("text", "plain", StandardCharsets.UTF_8)),
+                            content().string(Base64.toBase64String(fileResource.getContentAsByteArray())));
+
+        } finally {
+            //убрать созданный файл
+            resourceService.deleteResource(uuidTestForRemoval.get());
+        }
     }
 
     @Test
