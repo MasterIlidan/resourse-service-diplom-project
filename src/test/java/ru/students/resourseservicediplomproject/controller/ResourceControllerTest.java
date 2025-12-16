473e048c3c58b09be4812ff527fcd8d38d88d02a
MasterIlidan <segamil2001@gmail.com>
2025-12-16 18:04:29 +0500

    Тест delete метода API

--- src/test/java/ru/students/resourseservicediplomproject/controller/ResourceControllerTest.java
+++ src/test/java/ru/students/resourseservicediplomproject/controller/ResourceControllerTest.java
@@ -1,10 +1,7 @@
 package ru.students.resourseservicediplomproject.controller;
 
-import jakarta.transaction.Transactional;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.postgresql.shaded.com.ongres.scram.common.bouncycastle.base64.Base64;
-import org.postgresql.shaded.com.ongres.scram.common.bouncycastle.base64.Base64Encoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,19 +9,15 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import ru.students.resourseservicediplomproject.service.ResourceService;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -113,12 +106,45 @@ class ResourceControllerTest {
 
 
     @Test
-    void deleteResource_returnWithStatusOk() {
+    void deleteResource_returnWithStatusOk() throws Exception {
+        Resource fileResource = new ClassPathResource("./testImage.png");
+        MockMultipartFile firstFile = new MockMultipartFile(
+                "image", fileResource.getFilename(),
+                MediaType.MULTIPART_FORM_DATA_VALUE,
+                fileResource.getInputStream());
+        AtomicReference<String> uuidTestForRemoval = new AtomicReference<>();
+        try {
+            mockMvc.perform(multipart("/resource")
+                            .file(firstFile))
+                    .andExpectAll(
+                            status().isCreated(),
+                            (res) -> {
+                                assertDoesNotThrow(() -> {
+                                    UUID.fromString(res.getResponse().getContentAsString());
+                                });
+                            },
+                            (res) -> {
+                                uuidTestForRemoval.set(res.getResponse().getContentAsString());
+                            });
 
+            mockMvc.perform(
+                            delete("/resource" + uuidTestForRemoval.get())
+                                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpectAll(
+                            status().isOk());
+
+        } finally {
+            //убрать созданный файл
+            resourceService.deleteResource(uuidTestForRemoval.get());
+        }
     }
 
     @Test
-    void deleteResource_returnWithStatusNotFound() {
-
+    void deleteResource_returnWithStatusNotFound() throws Exception {
+        mockMvc.perform(
+                        delete("/resource" + UUID.randomUUID())
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpectAll(
+                        status().isNotFound());
     }
 }
