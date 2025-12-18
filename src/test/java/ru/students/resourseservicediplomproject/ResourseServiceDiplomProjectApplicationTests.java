f45ff9fe217d0ccaa5aaaec51510ac18754513cd
MasterIlidan <segamil2001@gmail.com>
2025-12-18 16:26:01 +0500

    Тест теперь использует тестовую конфигурацию

--- src/test/java/ru/students/resourseservicediplomproject/ResourseServiceDiplomProjectApplicationTests.java
+++ src/test/java/ru/students/resourseservicediplomproject/ResourseServiceDiplomProjectApplicationTests.java
@@ -2,8 +2,10 @@ package ru.students.resourseservicediplomproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-development.properties")
 class ResourseServiceDiplomProjectApplicationTests {
 
     @Test
